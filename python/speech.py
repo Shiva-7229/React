0e318cdd0920dbad07ae85c1e52a1cf0bc428e9f
Shiva-7229 <bogasadashivudu@gmail.com>
2024-03-31 22:45:25 +0530

    Python Added

---
+++ python/speech.py
@@ -0,0 +1,9 @@
+"""
+        *** Speech Recognition ***
+            -- setup python latest version
+            -- add pip to environment variables
+            Installing Packages ==>
+                               - pip install SpeechRecognition
+                               - pip install gTTS
+                               - pip install playsound
+"""
