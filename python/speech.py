ce3e41468504a35e7f9dfdb1ebec294bd8d5f110
Shiva-7229 <bogasadashivudu@gmail.com>
2024-04-13 14:26:31 +0530

    speech added

--- python/speech.py
+++ python/speech.py
@@ -6,4 +6,22 @@
                                - pip install SpeechRecognition
                                - pip install gTTS
                                - pip install playsound
+                               - pip install pyttsx3
+                               - pip install pyaudio
 """
+
+import speech_recognition
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer() 
+while True:
+        with speech_recognition.Microphone() as mic: 
+            recognizer.adjust_for_ambient_noise(mic, duration=0.4) 
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            print(f"Recognized {text}")
+
+    
