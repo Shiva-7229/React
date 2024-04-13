ac80a386140c936dfa9ca1b1d7364c89dc4795b0
Shiva-7229 <bogasadashivudu@gmail.com>
2024-04-14 00:14:48 +0530

    speech ended

--- python/speech.py
+++ python/speech.py
@@ -10,18 +10,28 @@
                                - pip install pyaudio
 """
 
-import speech_recognition
+import speech_recognition as sr
 import pyttsx3
 
-recognizer = speech_recognition.Recognizer() 
-while True:
-        with speech_recognition.Microphone() as mic: 
+r = sr.Recognizer() 
+"""while True:
+        with sr.Microphone() as mic: 
             recognizer.adjust_for_ambient_noise(mic, duration=0.4) 
             audio = recognizer.listen(mic)
 
             text = recognizer.recognize_google(audio)
             text = text.lower()
 
-            print(f"Recognized {text}")
+            print(f"Recognized {text}")"""
 
+"""def speekText(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()"""
+with sr.Microphone() as mic:
+    print("Silence Please, calibrating background noise")
+    r.adjust_for_ambient_noise(mic, duration=2)
+    print("calibrated, Now speak.....")
+
+#speekText("Hello World")
     
